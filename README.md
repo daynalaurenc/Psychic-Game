60ff1017192d5f4521fc9e76972f76fd6cf531f5
daynalaurenc <daynalaurenc@gmail.com>
2019-01-06 13:21:57 -0800

    JS is working, ReadMe completed, need CSS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 ### Psychic-Game
 
-The Psychic-Game is a game utilizing simple HTML, to display the text and information, and some JavaScript code, to detect the user's input and react accordingly.
+The Psychic-Game is a game utilizing simple HTML and JavaScript. The HTML displays the text and information, while the JavaScript detects the user's input and reacts accordingly. 
 
 This project is useful because it allows students a hands-on approach to incorporate concepts learned in class lectures and activities.
 
@@ -16,15 +16,8 @@ If Player's letter choice matches the Computer correctly, Player wins the game.
 If Player exhausts 9 guesses, Player loses the game.
 Game will automatically refresh once Player wins or loses.
 
-**NOTE:** A `for-loop` is not technically necessary to make Psychic-Game game, but could be used. Focus on the topics we have just covered when thinking about how to build out the game.
+* **NOTE:** When coding this game, a `for-loop` is not technically necessary to make Psychic-Game game, but could be used. 
 
-* **NOTE:** Don’t use `document.write` as it will delete the contents of your page including your JavaScript. Use  `document.getElementById`, alongside `innerHTML`, to write to the DOM.
+* **NOTE:** When coding this game, don’t use `document.write` as it will delete the contents of your page including your JavaScript. Use  `document.getElementById`, alongside `innerHTML`, to write to the DOM.
 
-
-**NOTE:** 
-**NOTE:** 
-What the project does
-Why the project is useful
-How users can get started with the project
-Where users can get help with your project
-Who maintains and contributes to the project
+* **NOTE:** When coding this game, breathe deeply, drink water, and check your usage of proper syntax and spacing. Enjoy!
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,5 @@
 
-var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w","x", "y", "z"];
             
 var wins = 0;
 var losses = 0;
@@ -26,70 +26,15 @@ document.onkeypress = function(event) {
         resetGame();
     }
 
-    document.getElementById("win").innerHTML = "Wins: " + wins;
-    document.getElementById("loss").innerHTML = "Losses: " + losses;
+    document.getElementById("wins").innerHTML = "Wins: " + wins;
+    document.getElementById("losses").innerHTML = "Losses: " + losses;
     document.getElementById("guesses").innerHTML = "Guesses Left: " + guesses;
-    document.getElementById("guessedletters").innerHTML = "Your Guesses so far: " + guessedLetters;
+    document.getElementById("guessedLetters").innerHTML = "Your Guesses so far: " + guessedLetters;
 }
 
 function resetGame() {
     guesses = 9;
     guessedLetters = [];
-    computerChoice = alphabet[Math.floor(math.random() * alphabet.length)];
+    computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+    console.log("guessedLetter" + computerChoice);
 }
-
-
-    
-            
-// // Create variables that hold references to the places in the HTML where we want to display things.
-//     var directionsText = document.getElementById("directions-text");
-//     // var computerChoiceText = document.getElementById("computerchoice-text");
-//     var winsText = document.getElementById("wins-text");
-//     var lossesText = document.getElementById("losses-text");
-//     var guessLeftText = document.getElementById("guessLeft-text");
-//     var userChoiceText = document.getElementById("userchoice-text");
-            
-// // This function is run whenever the user presses a key.
-//     document.onkeyup = function(event) {
-            
-// // Determines which key was pressed.
-//     var userGuess = event.key;
-            
-// // Randomly chooses a choice from the options array. This is the Computer's guess.
-//     var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
-            
-// // Reworked our code from last step to use "else if" instead of lots of if statements.
-            
-// // This logic determines the outcome of the game (win/loss/tie), and increments the appropriate number
-//     // if ((userGuess === "r") || (userGuess === "p") || (userGuess === "s")) {
-            
-//     // if ((userGuess === "r") && (computerGuess === "s")) {
-//     //     wins++;
-//     // } else if ((userGuess === "r") && (computerGuess === "p")) {
-//     //     losses++;
-//     // } else if ((userGuess === "s") && (computerGuess === "r")) {
-//     //     losses++;
-//     // } else if ((userGuess === "s") && (computerGuess === "p")) {
-//     //     wins++;
-//     // } else if ((userGuess === "p") && (computerGuess === "r")) {
-//     //     wins++;
-//     // } else if ((userGuess === "p") && (computerGuess === "s")) {
-//     //     losses++;
-//     // } else if (userGuess === computerGuess) {
-//     //     ties++;
-//     // }
-            
-// // Hide the directions
-//     directionsText.textContent = "";
-            
-// // Display the user and computer guesses, and wins/losses/ties.
-//     userChoiceText.textContent = "You chose: " + userGuess;
-//     computerChoiceText.textContent = "The computer chose: " + computerGuess;
-//     winsText.textContent = "wins: " + wins;
-//     lossesText.textContent = "losses: " + losses;
-//     tiesText.textContent = "ties: " + ties;
-//     }
-         
-// };
-//               </script>
-            
